8e6585829bd5500d5a26525d5abe2238ec06521a
Jhonny Hilário Goulart <jhonnyhilarioti@gmail.com>
2023-03-07 12:02:23 -0300

    Sobrescrita do método toString

---
+++ src/com/maratonajava/javacore/sobrescrita/test/AnimeTest.java
@@ -0,0 +1,12 @@
+package com.maratonajava.javacore.sobrescrita.test;
+
+import com.maratonajava.javacore.sobrescrita.model.Anime;
+
+public class AnimeTest {
+	public static void main(String[] args) {
+		Anime anime = new Anime("Naruto");
+		
+		System.out.println(anime);
+			
+	}
+}
