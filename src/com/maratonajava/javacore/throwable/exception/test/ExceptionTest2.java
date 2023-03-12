1b9532f3a1d68fd78a39b0729fa360901f112736
Jhonny Hilário Goulart <jhonnyhilarioti@gmail.com>
2023-03-12 15:30:16 -0300

    Lançando um exception do tipo checked utilizando THROWS

---
+++ src/com/maratonajava/javacore/throwable/exception/test/ExceptionTest2.java
@@ -0,0 +1,23 @@
+package com.maratonajava.javacore.throwable.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest2 {
+	public static void main(String[] args) throws IOException {
+
+		criarNovoArquivo();
+
+	}
+
+	public static void criarNovoArquivo() throws IOException {
+		File file = new File("arquivo\\teste.txt");
+		try {
+			boolean isCriado = file.createNewFile();
+			System.out.println("Arquivo criado? " + isCriado);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+}
