ec11bfaafc20361b750595358567db6f82b3a3eb
Jhonny Hilário Goulart <jhonnyhilarioti@gmail.com>
2023-03-17 15:48:07 -0300

    Alguns métodos da classe Locale

---
+++ src/com/maratonajava/javacore/datas/formatacao/test/LocaleTest02.java
@@ -0,0 +1,21 @@
+package com.maratonajava.javacore.datas.formatacao.test;
+
+import java.util.Locale;
+
+public class LocaleTest02 {
+	public static void main(String[] args) {
+		System.out.println(Locale.getDefault());
+		String[] isoCountries = Locale.getISOCountries();
+		String[] isoLanguages = Locale.getISOLanguages();
+
+		for (String isoCountrie : isoCountries) {
+			System.out.print(isoCountrie + " ");
+		}
+
+		System.out.println();
+
+		for (String isoLanguage : isoLanguages) {
+			System.out.print(isoLanguage + " ");
+		}
+	}
+}
